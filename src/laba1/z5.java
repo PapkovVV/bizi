41e6c1e9cebbbc20de65e21f37f48fee672b72c2
Valentine <getgggg2@gmail.com>
2023-03-29 09:38:27 +0300

    second

---
+++ src/laba1/z5.java
@@ -0,0 +1,14 @@
+package laba1;
+import java.util.Scanner;
+
+public class z5 {
+    public static void main(String[] args)
+    {
+        int first=44;
+        while(first<=99)
+        {
+            System.out.print(first+"\t");
+            first++;
+        }
+    }
+}
